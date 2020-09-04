aa15f2ac8df3c0f0cdf7e37f93100a14c4d2e623
Md. Ashraful Mollah <ashrafulash@github.com>
2020-09-04 08:06:54 +0600

    added favicon

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="<?php echo get_template_directory_uri(  ) ?>/assets/images/favicon.png" type="image/png" sizes="16x16">
+</head>
+<body>
+
+<?php  get_template_directory_uri(  ) ?>
+
+<img src="<?php echo get_template_directory_uri(  ) ?>/assets/images/favicon.png" alt="">
+    
